268fe60158605c5afa6e255e59c12d0f0ec1e244
nobody822 <186195481+syntax-error002@users.noreply.github.com>
2026-01-08 12:12:07 +0530

    feat: improve game of life UX with controls and resize fix

--- day-19-game-of-life/sketch.js
+++ day-19-game-of-life/sketch.js
@@ -2,6 +2,8 @@ let grid;
 let cols;
 let rows;
 let resolution = 8;
+let isRunning = true;
+let speedSlider;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -10,11 +12,16 @@ function setup() {
     grid = createEmptyGrid();
     randomizeGrid();
     background(0);
+
+    speedSlider = select('#speedSlider');
 }
 
 function draw() {
+    // Control speed
+    frameRate(parseInt(speedSlider.value()));
+
     // Semi-transparent background creates the "trailing" effect
-    background(0, 40); 
+    background(0, 40);
 
     if (mouseIsPressed) {
         let x = floor(mouseX / resolution);
@@ -34,28 +41,46 @@ function draw() {
         }
     }
 
-    let next = createEmptyGrid();
+    if (isRunning) {
+        let next = createEmptyGrid();
 
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            let state = grid[i][j];
-            let neighbors = countNeighbors(grid, i, j);
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                let state = grid[i][j];
+                let neighbors = countNeighbors(grid, i, j);
 
-            if (state == 0 && neighbors == 3) {
-                next[i][j] = 1;
-            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-                next[i][j] = 0;
-            } else {
-                next[i][j] = state;
+                if (state == 0 && neighbors == 3) {
+                    next[i][j] = 1;
+                } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                    next[i][j] = 0;
+                } else {
+                    next[i][j] = state;
+                }
             }
         }
+        grid = next;
     }
-    grid = next;
 }
 
 function keyPressed() {
-    if (key === 'c' || key === 'C') grid = createEmptyGrid();
+    if (key === 'c' || key === 'C') clearGrid();
     if (key === 'r' || key === 'R') randomizeGrid();
+    if (key === ' ' || key === 'p' || key === 'P') togglePlay();
+}
+
+function togglePlay() {
+    isRunning = !isRunning;
+    let btn = select('#toggleBtn');
+    if (isRunning) {
+        btn.html('Pause');
+    } else {
+        btn.html('Play');
+    }
+}
+
+function clearGrid() {
+    grid = createEmptyGrid();
+    background(0);
 }
 
 function createEmptyGrid() {
@@ -72,6 +97,7 @@ function randomizeGrid() {
             grid[i][j] = floor(random(2));
         }
     }
+    background(0);
 }
 
 function countNeighbors(grid, x, y) {
@@ -88,6 +114,20 @@ function countNeighbors(grid, x, y) {
 }
 
 function windowResized() {
+    let oldGrid = grid;
+    let oldCols = cols;
+    let oldRows = rows;
+
     resizeCanvas(windowWidth, windowHeight);
-    setup();
+    cols = floor(width / resolution);
+    rows = floor(height / resolution);
+    grid = createEmptyGrid();
+
+    // Attempt to copy over old state
+    for (let i = 0; i < min(oldCols, cols); i++) {
+        for (let j = 0; j < min(oldRows, rows); j++) {
+            grid[i][j] = oldGrid[i][j];
+        }
+    }
+    background(0);
 }
